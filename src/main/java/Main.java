a0353bd6afa22c24d56bbd6a0b2401b59bdb3618
kleimenhoffer <czarion@proton.me>
2026-06-21 01:31:24 +0530

    implemented PATH search for type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,24 +14,37 @@ public class Main {
             if (input.equals("exit")) {
                 break;
             } else if (input.startsWith("echo ")) {
-                String message = input.substring(5);
-                System.out.println(message);
+                System.out.println(input.substring(5));
             } else if (input.equals("echo")) {
                 System.out.println();
-            }
-
-            else if (input.startsWith("type ")) {
-
+            } else if (input.startsWith("type ")) {
                 String command = input.substring(5).trim();
 
                 if (command.equals("exit") || command.equals("echo") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
-                    System.out.println(command + ": not found");
-                }
-            }
 
-            else {
+                    String pathEnv = System.getenv("PATH");
+
+                    String[] folders = pathEnv.split(File.pathSeparator);
+
+                    boolean found = false;
+                    for (String folder : folders) {
+
+                        File file = new File(folder, command);
+
+                        if (file.exists() && Files.isExecutable(Paths.get(file.getAbsolutePath()))) {
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
+                }
+            } else {
                 System.out.println(input + ": command not found");
             }
         }

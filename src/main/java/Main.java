5465a821a0d95dd2825da2a6eed77c69893cd0e2
kleimenhoffer <czarion@proton.me>
2026-06-21 02:45:30 +0530

    implemented relative paths for cd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    // This is our "Virtual Tracker". It keeps track of where the shell "thinks" it
-    // is.
     private static String currentDirectory = System.getProperty("user.dir");
 
     public static void main(String[] args) throws Exception {
@@ -27,32 +25,36 @@ public class Main {
 
             if (command.equals("exit")) {
                 break;
-            }
-            // --- NEW LOGIC FOR CD STARTS HERE ---
-            else if (command.equals("cd")) {
+            } else if (command.equals("cd")) {
                 if (parts.length < 2) {
-                    // If user just types 'cd', we do nothing or could go to home
+
                 } else {
                     String newPath = parts[1];
+                    File targetDirectory;
 
-                    // We are only handling Absolute Paths for now (those starting with /)
                     if (newPath.startsWith("/")) {
-                        File directory = new File(newPath);
-                        if (directory.exists() && directory.isDirectory()) {
-                            // Update our tracker!
-                            currentDirectory = directory.getAbsolutePath();
+
+                        targetDirectory = new File(newPath);
+                    } else {
+
+                        targetDirectory = new File(currentDirectory, newPath);
+                    }
+
+                    try {
+
+                        String canonicalPath = targetDirectory.getCanonicalPath();
+                        File resolvedDir = new File(canonicalPath);
+
+                        if (resolvedDir.exists() && resolvedDir.isDirectory()) {
+                            currentDirectory = canonicalPath;
                         } else {
                             System.out.println("cd: " + newPath + ": No such file or directory");
                         }
-                    } else {
-                        // For this stage, we ignore relative paths like '..' or '.'
+                    } catch (Exception e) {
                         System.out.println("cd: " + newPath + ": No such file or directory");
                     }
                 }
-            }
-            // --- NEW LOGIC FOR CD ENDS HERE ---
-            else if (command.equals("pwd")) {
-                // Now we print the tracker variable, NOT the system property
+            } else if (command.equals("pwd")) {
                 System.out.println(currentDirectory);
             } else if (command.equals("echo")) {
                 StringBuilder message = new StringBuilder();
@@ -62,10 +64,9 @@ public class Main {
                 System.out.println(message.toString());
             } else if (command.equals("type")) {
                 if (parts.length < 2) {
-                    // Do nothing
+
                 } else {
                     String target = parts[1];
-                    // Added 'cd' to the list of builtins
                     if (target.equals("exit") || target.equals("echo") || target.equals("type") || target.equals("pwd")
                             || target.equals("cd")) {
                         System.out.println(target + " is a shell builtin");
@@ -82,10 +83,7 @@ public class Main {
                 String fullPath = findExecutableInPath(command);
                 if (fullPath != null) {
                     ProcessBuilder pb = new ProcessBuilder("sh", "-c", input);
-
-                    // CRUCIAL: This tells the external program to run inside our virtual folder
                     pb.directory(new File(currentDirectory));
-
                     pb.inheritIO();
                     Process process = pb.start();
                     process.waitFor();

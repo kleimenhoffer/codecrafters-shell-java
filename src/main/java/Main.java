ff6ce68e859750dc8a58f063baf70c64cb1eb15d
kleimenhoffer <czarion@proton.me>
2026-06-20 11:39:55 +0530

    implemented type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,15 +10,22 @@ public class Main {
 
             if (input.equals("exit")) {
                 break;
-            }
-
-            else if (input.startsWith("echo ")) {
-
+            } else if (input.startsWith("echo ")) {
                 String message = input.substring(5);
                 System.out.println(message);
             } else if (input.equals("echo")) {
-
                 System.out.println();
+            }
+
+            else if (input.startsWith("type ")) {
+
+                String command = input.substring(5).trim();
+
+                if (command.equals("exit") || command.equals("echo") || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
             }
 
             else {
